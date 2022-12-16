91a98ad1bab58592ff16809c87e11b74d75ac6cc
wildan1112 <wildanmalulazam@gmail.com>
2022-12-16 09:43:20 +0700

    sesi6

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'company_id'
+    ];
+
+    // relasi role->company
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    // relasi role->responsibility
+    public function responsibilities()
+    {
+       return $this->hasMany(Responsibilty::class);
+    }
+
+    // relasi role->employee
+    public function employees()
+    {
+       return $this->hasMany(Employee::class);
+    }
 }
--- app/Models/Team.php
+++ app/Models/Team.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Team extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'icon', 'company_id'
+    ];
+
+    // relasi team-company
+    public function company()
+    {
+        return $this->belongsTo(Comapny::class);
+    }
+
+    // relasi team-employee
+    public function employees()
+    {
+        return $this->hasMany(Employee::class);
+    }
 }
