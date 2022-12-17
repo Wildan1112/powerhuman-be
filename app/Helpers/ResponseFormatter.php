c2b8780ae249fc414204fd9cb69b1015f006d6c2
wildan1112 <wildanmalulazam@gmail.com>
2022-12-17 11:00:13 +0700

    sesi 7

--- app/Helpers/ResponseFormatter.php
+++ app/Helpers/ResponseFormatter.php
@@ -2,45 +2,30 @@
 
 namespace App\Helpers;
 
-/**
- * Format response.
- */
 class ResponseFormatter
 {
-    /**
-     * API Response
-     *
-     * @var array
-     */
     protected static $response = [
         'meta' => [
             'code' => 200,
             'status' => 'success',
             'message' => null,
         ],
-        'data' => null,
+        'result' => null,
     ];
 
-    /**
-     * Give success response.
-     */
     public static function success($data = null, $message = null)
     {
         self::$response['meta']['message'] = $message;
-        self::$response['data'] = $data;
+        self::$response['result'] = $data;
 
         return response()->json(self::$response, self::$response['meta']['code']);
     }
 
-    /**
-     * Give error response.
-     */
-    public static function error($data = null, $message = null, $code = 400)
+    public static function error($message = null, $code = 400)
     {
         self::$response['meta']['status'] = 'error';
         self::$response['meta']['code'] = $code;
         self::$response['meta']['message'] = $message;
-        self::$response['data'] = $data;
 
         return response()->json(self::$response, self::$response['meta']['code']);
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\Team;
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(7)->unverified()->create();
 
         // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        //     'name' => 'Azam uye',
+        //     'email' => 'azam@gmail.com',
         // ]);
+
+        // Company::factory(10)->create();
+        // Team::factory(30)->create();
+        // Role::factory(50)->create();
+        // Responsibility::factory(150)->create();
+        // Employee::factory(1000)->create();
+
     }
 }
